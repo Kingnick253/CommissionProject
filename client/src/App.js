718d236e926bddd5c02e4742b1bd8d77e398cf2d
Kingnick253 <nicholaswebb2016@gmail.com>
2023-12-03 07:30:20 -0800

    made some more ground work changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 import Home from "./pages/Home.js";
+import Navbar from './components/Navbar.js';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
        <Home />
     </div>
   );
