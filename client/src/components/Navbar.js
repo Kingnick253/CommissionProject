3ae1908eca5bd49dd20925640786ae3b3ad95b31
Kingnick253 <nicholaswebb2016@gmail.com>
2024-04-14 22:05:51 -0700

    removed title and fixed error with babel

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,23 @@
 import React from "react";
-import Logo from "../assets/creditgeeks-1.png"
+import Logo from "../assets/creditgeeks-1.png";
 const Navbar = () => {
   return (
-    <div className="bg-zinc-500 text-white h-20 fixed w-full flex items-center px-4">
+    <div className="bg-zinc-500 text-white h-20 fixed w-full flex items-center px-4 justify-content-between">
       {/* Logo */}
       <div className="flex-shrink-0">
         {/* Add your logo component or image here */}
-        <img src={Logo} alt="Logo" className="h-20 w-20 mb-4" />
+        <img src={Logo} alt="Logo" className="h-20 w-20 mb-1 shadow-lg" />
       </div>
 
       {/* Title */}
-      <div className="flex-grow justify-between text-center font-bold text-lg">
+      {/* <div className="flex-grow text-center font-bold px-10 text-lg">
         Credit Geeks
-      </div>
+      </div> */}
 
       {/* Navigation links or additional content */}
-      <div className='flex-grow'>
-        {/* Add your navigation links or other content here */}
-      </div>
+      {/* <div className="flex-grow">
+   
+      </div> */}
     </div>
   );
 };
